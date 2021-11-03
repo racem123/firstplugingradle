2fe26ae7d69a9bf9387601537ba1d299a6195b17
Racem NAIFAR <rnaifar@freedomofdev.com>
2021-11-03 15:09:31 +0100

    Initial commit

---
+++ demoGradle/build/docs/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.example.gradle","c":"MyPlugin","l":"apply(Project)","u":"apply(org.gradle.api.Project)"},{"p":"org.example.gradle","c":"MyPlugin","l":"MyPlugin()","u":"%3Cinit%3E()"},{"p":"org.example.gradle","c":"MyTask","l":"myTask()"},{"p":"org.example.gradle","c":"MyTask","l":"MyTask()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ demoGradle/build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"org.example.gradle","l":"MyPlugin"},{"p":"org.example.gradle","l":"MyTask"}];updateSearchResults();
